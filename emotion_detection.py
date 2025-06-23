6a58658afb671d827ca5445446839361c8bcb138
rapalank <qwanta@gmail.com>
2025-06-23 19:12:28 -0400

    second commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -13,4 +13,8 @@ def emotion_detector(text_to_analyse : str):
         print(f"HTTP error occurred: {e}")
     except requests.exceptions.RequestException as e:
         print(f"An unexpected Requests error occurred: {e}")
-    return response.text
+
+    emotion_scores = response.json()['emotionPredictions'][0]['emotion']
+    highest_value_key = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = highest_value_key 
+    return emotion_scores
